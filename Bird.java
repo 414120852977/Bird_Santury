abe4634a0651dbeab51e886c5cce4cc0d8df6494
Ashok Mane <ashokmane81800@gmail.com>
2021-04-11 14:36:37 +0530

    [ashok] created a java bird to get an access to each bird from this file all other files extends with this file

---
+++ Bird.java
@@ -0,0 +1,8 @@
+package birdsandsantury;
+
+public abstract class Bird  {
+	public abstract void eat(); 
+	abstract void incrementcount();
+	public abstract int getCount();
+	public abstract void decrementCount();
+}
